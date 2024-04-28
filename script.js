a961e689127c1613a83b0c3c22c8840c2ad4ab38
pradoshcodes <pradoshmohanty034@gmail.com>
2024-04-28 22:04:23 +0530

    gym website

---
+++ script.js
@@ -0,0 +1,8 @@
+    let registration=document.querySelector('.register');
+    let namefield=document.querySelector('.name');
+    let headtitle=document.querySelector('.title');
+
+
+registration.addEventListener('click',()=>{
+    namefield.style.maxHeight='0';
+} )
