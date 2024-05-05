1fc00ddb82c9b5de5475273aaf5a616249adb1d8
pradoshcodes <pradoshmohanty034@gmail.com>
2024-05-05 22:44:16 +0530

    Gym website

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-    let registration=document.querySelector('.register');
-    let namefield=document.querySelector('.name');
-    let headtitle=document.querySelector('.title');
+var nameError=document.getElementById('name-error');
+var emailError=document.getElementById('email-error');
+var passwordError=document.getElementById('password-error');
+var repasswordError=document.getElementById('repassword-error');
 
-
-registration.addEventListener('click',()=>{
-    namefield.style.maxHeight='0';
-} )
+function validateName(){
+     var name=document.getElementById('contact-name').value;
+     if(name.length==0){
+         nameError.innerHTML="Name is required";
+         return false; 
+     } 
+   if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+    nameError.innerHtml='write full name';
+    return false;
+   }
+   nameError.innerHTML='valid';
+   return true;
+}
